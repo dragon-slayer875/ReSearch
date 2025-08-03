70ca538972b675d8c67ae29b673edd0d35f6f35e
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-08-03 13:12:44 +0530

    code: initiate module and add redis constructor
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

---
+++ backend/indexer/internals/queue/redis.go
@@ -0,0 +1,21 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient(redisUrl string) (*redis.Client, error) {
+	redisOpts, err := redis.ParseURL(redisUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse Redis URI: %w", err)
+	}
+	redisClient := redis.NewClient(redisOpts)
+
+	if err := redisClient.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
+	}
+
+	return redisClient, nil
+}
