e15c862373e8d0c73ef59aa14409e6de0995ddd8
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-10-06 02:05:57 +0530

    indexer(code): implement logic to run indexer
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

---
+++ backend/indexer/cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"indexer/internals/config"
+	"indexer/internals/indexer"
+	"indexer/internals/queue"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "Path to configuration file")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "indexer: ", log.LstdFlags|log.Lshortfile)
+
+	ctx := context.Background()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		logger.Fatalln("Failed to load config:", err)
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		logger.Fatalln("Error loading environment variables:", err)
+	}
+
+	redisClient, err := queue.NewRedisClient(os.Getenv("REDIS_URL"))
+	if err != nil {
+		logger.Fatalf("Failed to connect to Redis: %v", err)
+	}
+
+	dbPool, err := pgxpool.New(ctx, os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		logger.Fatalf("Failed to connect to PostgreSQL: %v", err)
+	}
+
+	defer dbPool.Close()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-sigChan
+		logger.Println("Received shutdown signal, exiting...")
+		os.Exit(0)
+	}()
+
+	indexer := indexer.NewIndexer(logger, cfg.Indexer.WorkerCount, redisClient, dbPool, ctx)
+
+	logger.Println("Starting...")
+	indexer.Start()
+}
