310e42126b7cc26aff5cc505c99ff2b810c3300c
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-02-03 21:45:50 +0530

    server(feat): implement auth for submission acceptance and rejection endpoints
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- backend/server/internals/router/main.go
+++ backend/server/internals/router/main.go
@@ -1,10 +1,14 @@
 package router
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"os"
 	"server/internals/handlers"
 	"server/internals/services"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/keyauth"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
@@ -22,9 +26,21 @@ func searchRouter(app fiber.Router, service *services.LinksService) {
 }
 
 func crawlerBoardRouter(app fiber.Router, service *services.CrawlerBoardService) {
+	authMiddleware := keyauth.New(keyauth.Config{
+		Validator: func(c fiber.Ctx, key string) (bool, error) {
+			hashedAPIKey := sha256.Sum256([]byte(os.Getenv("ADMIN_KEY")))
+			hashedKey := sha256.Sum256([]byte(key))
+
+			if subtle.ConstantTimeCompare(hashedAPIKey[:], hashedKey[:]) == 1 {
+				return true, nil
+			}
+			return false, keyauth.ErrMissingOrMalformedAPIKey
+		},
+	})
+
 	app.Get("/submissions", handlers.GetSubmissions(service))
 	app.Post("/submissions", handlers.PostSubmissions(service))
 	app.Post("/submissions/votes", handlers.Vote(service))
-	app.Post("/submissions/accept", handlers.AcceptSubmissions(service))
-	app.Post("/submissions/reject", handlers.RejectSubmissions(service))
+	app.Post("/submissions/accept", authMiddleware, handlers.AcceptSubmissions(service))
+	app.Post("/submissions/reject", authMiddleware, handlers.RejectSubmissions(service))
 }
